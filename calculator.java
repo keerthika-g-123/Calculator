9672ddece7ff33d29d35f91c568adcbc787948a3
keerthika-g-123 <gkeerthikagovindaraj@gmail.com>
2025-05-20 12:39:08 +0530

    java program
    i create a code to perform calculation

---
+++ calculator.java
@@ -0,0 +1,13 @@
+import java.uti.*;
+class calculator
+{
+public static void main(String[] args)
+  {
+   int a=2;
+    int b=9;
+    System.out.println("The Addition value:"+a+b);
+     System.out.println("The subtraction value:"+a-b);
+     System.out.println("The multiplication value:"+a*b);
+     System.out.println("The Dicision value: "+a%b);
+  }
+}
